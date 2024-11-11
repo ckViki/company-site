43379da72b2eb1f3c742a1100dbaa110a3a1617d
vignesh kumar C <>
2024-11-11 23:35:48 +0530

    updated the service page

--- src/routes/services/+page.svelte
+++ src/routes/services/+page.svelte
@@ -13,36 +13,47 @@
 			<div class="text-[50px] text-center font-semibold">
 				We’ve got everything you need to launch and grow your business
 			</div>
-		</div>
-		<hr class="opacity-50 max-w-[40vw]" />
-
-		<div class="text-[24px] m-auto font-serif px-5 text-center">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-			 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-			 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt.
+			<div class="px-[250px] my-5">
+				<hr class="opacity-50" />
+			</div>
 		</div>
 	</div>
 
-	<div class="flex gap-4 justify-between max-w-[1000px] pb-20">
-		<div class="border-r px-10 text-center border-gray-300 border-opacity-50">
-			<span class="text-[84px] text-white font-semibold">127</span> <br/>
-			<span class="text-[18px] font-semibold">Awards Received</span>
+	
+	<div class="grid justify-items-stretch md:grid-cols-2 gap-5 max-w-[1000px] pb-24">
+		<div class="w-full items-center">
+			<span class="font-semibold  text-[40px] tracking-[0.01em]">Web Development</span>
+			<div class="text-[16px] px-2 tracking-[0.05em] font-serif text-gray-400">
+				Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.
+			</div>
+			
 		</div>
 
-		<div class="border-r px-10 text-center border-gray-300 border-opacity-50">
-			<span class="text-[84px] text-white font-semibold">172</span> <br/>
-			<span class="text-[18px] font-semibold">Cups of Coffee</span>
+		<div class="w-full items-center">
+			<span class="font-semibold  text-[40px] tracking-[0.01em]">Web Design</span>
+			<div class="text-[16px] px-2 tracking-[0.05em] font-serif text-gray-400">
+				Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.
+			</div>
+			
 		</div>
 
-		<div class="border-r px-10 text-center border-gray-300 border-opacity-50">
-			<span class="text-[84px] text-white font-semibold">177</span> <br/>
-			<span class="text-[18px] font-semibold">Projects Completed</span>
+		<div class="w-full items-center">
+			<span class="font-semibold  text-[40px] tracking-[0.01em]">Cloud</span>
+			<div class="text-[16px] px-2 tracking-[0.05em] font-serif text-gray-400">
+				Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.
+			</div>
+			
 		</div>
 
-		<div class="px-10 text-center">
-			<span class="text-[84px] text-white font-semibold">111</span> <br/>
-			<span class="text-[18px] font-semibold">Happy Clients</span>
+		<div class="w-full items-center">
+			<span class="font-semibold  text-[40px] tracking-[0.01em]">Logo Design</span>
+			<div class="text-[16px] px-2 tracking-[0.05em] font-serif text-gray-400">
+				Nemo cupiditate ab quibusdam quaerat impedit magni. Earum suscipit ipsum laudantium. Quo delectus est. Maiores voluptas ab sit natus veritatis ut. Debitis nulla cumque veritatis. Sunt suscipit voluptas ipsa in tempora esse soluta sint.
+			</div>
+			
 		</div>
+
+		
 	</div>
+	
 </div>
